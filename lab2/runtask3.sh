12e8995f5c78bfa673d8175f9e20c476942ad533
Bo Song of group cpsc424 <bs744@compute-14-1.local>
2016-02-09 20:25:20 -0500

    finish lab2

---
+++ lab2/runtask3.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#PBS -l procs=4,tpn=2,mem=68gb
+#PBS -l walltime=2:00
+#PBS -N Task3
+#PBS -r n
+#PBS -j oe
+#PBS -q cpsc424
+
+# Load necessary module files
+module load Langs/Intel/14 MPI/OpenMPI/1.6.5
+
+# Print initial working directory
+pwd
+
+# Change to submission directory
+cd $PBS_O_WORKDIR
+
+# Now print the current working directory
+pwd
+
+# Print the node list
+cat $PBS_NODEFILE
+
+# Run the program 3 times
+time mpiexec -n 4 task3
+time mpiexec -n 4 task3
+time mpiexec -n 4 task3
