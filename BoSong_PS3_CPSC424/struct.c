2a5b0a973a7562c3438d6a5d7ddd75d10886c3e6
bo song <bo.song@yale.edu>
2016-02-25 15:41:33 -0500

    add struct

---
+++ BoSong_PS3_CPSC424/struct.c
@@ -0,0 +1,52 @@
+#include "stdio.h"
+#include "mpi.h"
+#include "stdlib.h"
+#include "stddef.h"
+
+typedef struct car_s{
+	int length;
+	double weight;
+	int color[3];
+}car;
+int main(int argc, char ** argv){
+	const int tag = 13;
+	int size, rank;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	const int nitems = 3;
+	// how many elements in each block
+	int blocklength[nitems] = {1, 2, 3};
+	MPI_Datatype types[nitems] = {MPI_INT, MPI_DOUBLE, MPI_INT};
+	MPI_Datatype mpi_car_type;
+	MPI_Aint offsets[nitems];
+
+	offsets[0] = offsetof(car, length);
+	offsets[1] = offsetof(car, weight);
+	offsets[2] = offsetof(car, color);
+
+	MPI_Type_create_struct(nitems, blocklength, offsets, types, &mpi_car_type);
+	MPI_Type_commit(&mpi_car_type);
+
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Barrier(MPI_COMM_WORLD);
+	if(rank == 0){
+		car send;
+		car.length = 10;
+		car.weight = 23.4;
+		car.color = {1,2,3};
+		const int dest = 1;
+		MPI_Send(&send, 1, mpi_car_type, dest, tag, MPI_COMM_WORLD);
+		printf("Master finish\n");
+	}
+	else{
+		MPI_Status status;
+		car recv;
+		const int src = 0;
+		MPI_Recv(&recv, 1, mpi_car_type, src, tag, MPI_COMM_WORLD);
+		printf("Process %d Recieved: length = %d, weight = %f, color[0] = %d, color[1] = %d, color[2] = %d",
+			rank, recv.length, recv.weight, recv.color[0], recv.color[1], recv.color[2]);
+	}
+	MPI_Type_free(&mpi_car_type);
+	MPI_Finalize();
+
+}
